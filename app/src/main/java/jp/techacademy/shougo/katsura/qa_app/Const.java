367af5ba4d49eece3bb5f1e379aa49f0a75ab434
Gomagoma1004 <32089543+Gomagoma1004@users.noreply.github.com>
2018-03-20 11:21:44 +0900

    アカウントが生成されません。

---
+++ app/src/main/java/jp/techacademy/shougo/katsura/qa_app/Const.java
@@ -0,0 +1,10 @@
+package jp.techacademy.shougo.katsura.qa_app;
+
+
+
+public class Const {
+    public static final String UsersPATH = "users";
+    public static final String ContentsPATH = "contents";
+    public static final String AnswersPATH = "answers";
+    public static final String NameKEY = "name";
+}
